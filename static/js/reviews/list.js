20725e6f9840a93b49019a4c0204f05f7a20e6a5
Pragati Poudel <pragati.poudel37@gmail.com>
2023-11-21 20:37:27 -0500

    Fix minor bug on dialog box

--- static/js/reviews/list.js
+++ static/js/reviews/list.js
@@ -13,7 +13,7 @@ span.onclick = function() {
 }
 
 window.onclick = function(event) {
-    if (event.taget == modal) {
+    if (event.target == modal) {
         modal.style.display = "none";
     }
 }
