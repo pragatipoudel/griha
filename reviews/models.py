a714bcda7592ea249b8721088a96815c233503ee
Pragati Poudel <pragati.poudel37@gmail.com>
2023-11-21 20:37:27 -0500

    Add review form

--- reviews/models.py
+++ reviews/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Review(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=254)
-    comment = models.TextField(blank=True)
+    comment = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
+from .forms import ReviewForm
 from .models import Review
 
 def reviewsListPage(request):
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ReviewForm()
+
     reviews = Review.objects.all()
-    context = {'reviews': reviews}
+    context = {'reviews': reviews, 'form': form}
     return render(request, "reviews/list.html", context)
-
-    
-
 
