7738d8f8312094a9ccc52b250ed19fe0d6a46221
Pragati Poudel <pragati.poudel37@gmail.com>
2023-11-21 20:37:27 -0500

    Add virtual environment and css file

--- griha/settings.py
+++ griha/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'reviews.apps.ReviewsConfig',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
