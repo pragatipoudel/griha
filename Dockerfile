4828ab9e093519795d1113b645721cf751a96f99
Pragati Poudel <pragati.poudel37@gmail.com>
2023-11-26 13:48:46 -0500

    Add docker compose file

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,3 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . code
 WORKDIR /code
 
-EXPOSE 8000
-
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- griha/settings.py
+++ griha/settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
