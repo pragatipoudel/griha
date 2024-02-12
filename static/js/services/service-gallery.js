77ff6c4196da0d9bcdd17f13e97948667da761db
Pragati Poudel <pragati.poudel37@gmail.com>
2024-02-12 11:39:42 -0500

    Change the footer height and reposition contact bar

--- static/js/services/service-gallery.js
+++ static/js/services/service-gallery.js
@@ -2,7 +2,7 @@
 const serviceBlocks = document.querySelectorAll('.services > .service');
 
 serviceBlocks.forEach(serviceBlock => {
-    serviceBlock.addEventListener('click', () => {
+    serviceBlock.addEventListener('mouseover', () => {
         const activeServiceBlock = document.querySelector('.services > .service.active');
         activeServiceBlock.classList.remove('active');
         serviceBlock.classList.add('active');
