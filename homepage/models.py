2e93d9151c06cdb3e4894992f7292965ccf94b49
Pragati Poudel <pragati.poudel37@gmail.com>
2024-02-12 21:19:04 -0500

    Make navbar logo a separate entity from main logo

--- homepage/models.py
+++ homepage/models.py
@@ -5,6 +5,7 @@ class HomePageContent(models.Model):
     header_image = models.ImageField(upload_to="homepage/header/")
     header_video = models.FileField(upload_to="homepage/header/", blank=True)
     logo = models.ImageField(upload_to="homepage/header", blank=True)
+    nav_bar_logo = models.ImageField(upload_to="homepage/header", blank=True)
     fav_icon = models.ImageField(upload_to="homepage/header", blank=True)
 
     def __str__(self):
