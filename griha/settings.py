0cad227532dbdf3abb22616e94202c0c34903bc2
Pragati Poudel <pragati.poudel37@gmail.com>
2024-05-03 13:31:42 -0400

    Use manifest static files storage for unique css filenames

--- griha/settings.py
+++ griha/settings.py
@@ -135,6 +135,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+    },
+}
+
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
